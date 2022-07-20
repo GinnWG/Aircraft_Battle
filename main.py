aaf15939f18b778325c93909074540bdec3a56f4
jinwang <wj19930703@gmail.com>
2022-07-20 16:23:50 +0200

    background init

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 import sys
 import traceback
 
+from pygame.locals import *
+
+
 pygame.init()
 pygame.mixer.init()
 
@@ -9,4 +12,58 @@ background_size = width, height = 480, 700
 screen = pygame.display.set_mode(background_size)
 pygame.display.set_caption("Aircraft Battle_GG")
 
-background = pygame.image.load()
+background = pygame.image.load("resource/images/background.png").convert()
+
+# load game
+pygame.mixer.music.load("resource/sound/game_music.ogg")
+pygame.mixer.music.set_volume(0.2)
+bullet_sound = pygame.mixer.Sound("resource/sound/bullet.wav")
+bullet_sound.set_volume(0.2)
+bomb_sound = pygame.mixer.Sound("resource/sound/use_bomb.wav")
+bomb_sound.set_volume(0.2)
+supply_sound = pygame.mixer.Sound("resource/sound/supply.wav")
+supply_sound.set_volume(0.2)
+get_bomb_sound = pygame.mixer.Sound("resource/sound/get_bomb.wav")
+get_bomb_sound.set_volume(0.2)
+get_bullet_sound = pygame.mixer.Sound("resource/sound/get_bullet.wav")
+get_bullet_sound.set_volume(0.2)
+upgrade_sound = pygame.mixer.Sound("resource/sound/upgrade.wav")
+upgrade_sound.set_volume(0.2)
+enemy3_fly_sound = pygame.mixer.Sound("resource/sound/enemy3_flying.wav")
+enemy3_fly_sound.set_volume(0.2)
+enemy1_down_sound = pygame.mixer.Sound("resource/sound/enemy1_down.wav")
+enemy1_down_sound.set_volume(0.2)
+enemy2_down_sound = pygame.mixer.Sound("resource/sound/enemy2_down.wav")
+enemy2_down_sound.set_volume(0.2)
+enemy3_down_sound = pygame.mixer.Sound("resource/sound/enemy3_down.wav")
+enemy3_down_sound.set_volume(0.5)
+me_down_sound = pygame.mixer.Sound("resource/sound/me_down.wav")
+me_down_sound.set_volume(0.2)
+
+
+def main():
+    pygame.mixer.music.play(-1)
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.blit(background, (0, 0))
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
+    except:
+        traceback.print_exc()
+        pygame.quit()
+        input()
