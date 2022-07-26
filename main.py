31ce3675de6eb3377e4ad74bbe4ad3f082d37d30
jinwang <wj19930703@gmail.com>
2022-07-26 17:33:48 +0200

    enemies planes have something wrong

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import traceback
 
 from pygame.locals import *
 
+import enemy
 import myPlane
 
 pygame.init()
@@ -42,15 +43,51 @@ me_down_sound = pygame.mixer.Sound("resource/sound/me_down.wav")
 me_down_sound.set_volume(0.2)
 
 
+def add_small_enemies(group1, group2, num):
+    for i in range(num):
+        e1 = enemy.SmallEnemy(bg_size)
+        group1.add(e1)
+        group2.add(e1)
+
+
+def add_mid_enemies(group1, group2, num):
+    for i in range(num):
+        e2 = enemy.MidEnemy(bg_size)
+        group1.add(e2)
+        group2.add(e2)
+
+
+def add_big_enemies(group1, group2, num):
+    for i in range(num):
+        e3 = enemy.BigEnemy(bg_size)
+        group1.add(e3)
+        group2.add(e3)
+
+
 def main():
     pygame.mixer.music.play(-1)
 
     # generate my plane
     me = myPlane.MyPlane(bg_size)
     clock = pygame.time.Clock()
+
+    # generate enemy
+    enemies = pygame.sprite.Group()
+    # small
+    small_enemies = pygame.sprite.Group()
+    add_small_enemies(small_enemies, enemies, 15)
+    # middle
+    mid_enemies = pygame.sprite.Group()
+    add_mid_enemies(mid_enemies, enemies, 4)
+    # big
+    big_enemies = pygame.sprite.Group()
+    add_big_enemies(big_enemies, enemies, 2)
+
     running = True
     switch_image = True
 
+    # delay of image
+    delay = 100
 
     while running:
         for event in pygame.event.get():
@@ -72,26 +109,50 @@ def main():
 
         screen.blit(background, (0, 0))
 
-        # switch Plane image
+        # paint big enemy planes
+        for each in big_enemies:
+            each.move()
+            if switch_image:
+                screen.blit(each.image1, each.rect)
+            else:
+                screen.blit(each.image2, each.rect)
+            if each.rect.bottom > -50:
+                enemy3_fly_sound.play()
 
-        switch_image = not switch_image
-        if switch_image:
-            screen.blit(me.image1, me.rect)
-        else:
-            screen.blit(me.image2, me.rect)
+        # paint mid enemy planes
+        for each in mid_enemies:
+            each.move()
+            screen.blit(each.image, each.rect)
 
+        # paint big enemy planes
+        for each in small_enemies:
+            each.move()
+            screen.blit(each.image, each.rect)
 
-        pygame.display.flip()
+            # switch my Plane
+            if switch_image:
+                screen.blit(me.image1, me.rect)
+            else:
+                screen.blit(me.image2, me.rect)
 
-        clock.tick(60)
+            # delay switch image of plane
+            if not (delay % 5):
+                switch_image = not switch_image
 
+            delay -= 1
+            if not delay:
+                delay = 100
 
-if __name__ == "__main__":
-    try:
-        main()
-    except SystemExit:
-        pass
-    except:
-        traceback.print_exc()
-        pygame.quit()
-        input()
+            pygame.display.flip()
+
+            clock.tick(60)
+
+    if __name__ == "__main__":
+        try:
+            main()
+        except SystemExit:
+            pass
+        except:
+            traceback.print_exc()
+            pygame.quit()
+            input()
